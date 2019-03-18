05c20cd22892e80bbbecc39e8f02dd0ea5ee46e1
riswanmsm <riswan@tutorwizard.lk>
2019-03-18 18:53:45 +0530

    fully working recorder and replayer

--- app.js
+++ app.js
@@ -2,25 +2,35 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const fs = require('fs');
 const cors = require('cors');
+const mysql = require('mysql');
 
 const app = express();
 
-let readData, finalData;
+let readData;
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '135790',
+    database : 'tutorwizard_work'
+  });
 
 app.use(cors());
 
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit: '50mb', extended: true}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+
+
 
 app.get('/', (req, res, next) => {
     res.send('Welcome to rrweb api');
 });
 
 app.get('/record', (req, res, next) => {
-    res.sendFile(__dirname + '/event-recorder.html');
+    res.sendFile(__dirname + '/view/event-recorder.html');
 });
 
 app.get('/replay', (req, res, next) => {
-    res.sendFile(__dirname + '/event-replay.html');
+    res.sendFile(__dirname + '/view/event-replay.html');
 });
 
 app.get('/read', (req, res, next) => {
@@ -33,11 +43,32 @@ app.get('/read', (req, res, next) => {
 });
 
 app.post('/api', (req, res) => {
-    fs.appendFile('events.json', JSON.stringify(req.body), (err) => {
+    fs.appendFile('events.json', JSON.stringify(req.body) + ',', (err) => {
         if(err) throw err;
         console.log('events updated');
     })
     res.send("event received");
+});
+
+app.post('/user', (req, res) => {
+    if(req.body.token !== undefined){
+        connection.query(`SELECT user_id FROM temp_users WHERE token = ?`,[req.body.token], (error, userIds) => {
+            if (error){
+                res.status(400).send('mysql error');
+                console.log(error);
+            } else{
+                connection.query(`SELECT email from users WHERE id = ?`, userIds[0].user_id, (error, email) => {
+                        if(error){
+                            res.status(400).send('mysql error');
+                            console.log(error);
+                        }
+                        res.send(email);
+                });
+            }
+        });
+    }else{
+        res.status(400).send('no token error');
+    } 
 });
 
 app.listen(8102, () => {
